32d0f925dffdfe3279ef2f4a0f88c1a089517146
mylo <48256865+Milopadma@users.noreply.github.com>
2022-10-27 21:24:57 +0800

    open up floaterm

--- lua/remap.lua
+++ lua/remap.lua
@@ -5,16 +5,5 @@ nnoremap("<leader>ff", "<cmd>Telescope find_files<CR>")
 nnoremap("<leader>fg", "<cmd>Telescope live_grep<CR>")
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
--- for lspsaga
-nnoremap("<C-j>", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
-nnoremap("K", "<cmd>Lspsaga hover_doc<CR>")
-nnoremap("<gd>", "<cmd>Lspsaga lsp_finder<CR>")
-nnoremap("<C-k>", "<cmd>Lspsaga signature_help<CR>")
-nnoremap("<gp>", "<cmd>Lspsaga preview_definition<CR>")
-nnoremap("<gr>", "<cmd>Lspsaga rename<CR>")
-nnoremap("<leader>ca", "<cmd>Lspsaga code_action<CR>")
-nnoremap("<leader>rn", "<cmd>Lspsaga rename<CR>")
-nnoremap("<leader>fo", "<cmd>Lspsaga open_floaterm<CR>")
-nnoremap("<leader>fc", "<cmd>Lspsaga close_floaterm<CR>")
 
 -- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -11,7 +11,9 @@ saga.init_lsp_saga {
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', '<gd>', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
-vim.keymap.set('n', '<gp>', '<Cmd>Lspsaga preview_definition<CR>', opts)
-vim.keymap.set('n', '<gr>', '<Cmd>Lspsaga rename<CR>', opts)
+vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
+vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+vim.keymap.set('n', '<leader>ca', "<cmd>Lspsaga code_action<CR>", opts)
+vim.keymap.set('n', '<leader>to', "<cmd>Lspsaga open_floaterm<CR>", opts)
