958e9b5e2da083483cf38b74b6813abfe6368b75
mylo <48256865+Milopadma@users.noreply.github.com>
2022-11-04 13:51:07 +0800

    new plugins

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -106,7 +106,7 @@ local config = {
     -- enable or disable highlighting for extra plugins
     plugins = {
       aerial = true,
-      beacon = true,
+      beacon = false,
       bufferline = true,
       cmp = true,
       dashboard = true,
@@ -222,7 +222,28 @@ local config = {
       -- You can also add new plugins here as well:
       -- Add plugins, the packer syntax without the "use"
       -- copilot
-      { 'github/copilot.vim' }
+      { 'github/copilot.vim' },
+      
+      -- beacon
+      { 'danilamihailov/beacon.nvim' },
+
+      -- lightspeed 
+    { 'ggandor/lightspeed.nvim' },
+
+        -- smooth scrolling
+        { 'karb94/neoscroll.nvim', config = function() require('neoscroll').setup({
+    -- All these keys will be mapped to their corresponding default scrolling animation
+    mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+    hide_cursor = true,          -- Hide cursor while scrolling
+    stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+    respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+    cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+    easing_function = nil,       -- Default easing function
+    pre_hook = nil,              -- Function to run before the scrolling animation starts
+    post_hook = nil,             -- Function to run after the scrolling animation ends
+    performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+}) end }
       -- { "andweeb/presence.nvim" },
       -- {
       --   "ray-x/lsp_signature.nvim",
