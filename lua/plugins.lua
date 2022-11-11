a9490dba5c1bf1341f3578e352abebb1dc351457
mylo <48256865+Milopadma@users.noreply.github.com>
2022-11-11 14:24:59 +0800

    remap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,16 +19,16 @@ packer.startup(function(use)
 		requires = { { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" } },
 	}) -- file finder
 	use("ggandor/lightspeed.nvim") -- fast cursor movement
-	-- use 'L3MON4D3/LuaSnip' -- Snippets
-	use("glepnir/lspsaga.nvim") -- lsp UIs
+	-- use 'l3mon4d3/luasnip' -- snippets
+	use("glepnir/lspsaga.nvim") -- lsp uis
 	-- use 'hrsh7th/cmp-buffer' -- buffer source
-	-- use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-	-- use 'hrsh7th/nvim-cmp' -- Autocompletion
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }) -- Treesitter
-	use("jose-elias-alvarez/null-ls.nvim") -- null-ls for LSP
-	use("MunifTanjim/prettier.nvim") -- prettier plugin
+	-- use 'hrsh7th/cmp-nvim-lsp' -- lsp source for nvim-cmp
+	-- use 'hrsh7th/nvim-cmp' -- autocompletion
+	use({ "nvim-treesitter/nvim-treesitter", run = ":tsupdate" }) -- treesitter
+	use("jose-elias-alvarez/null-ls.nvim") -- null-ls for lsp
+	use("muniftanjim/prettier.nvim") -- prettier plugin
 	use("onsails/lspkind-nvim") -- vscode-like pictograms for neovim lsp completion items
-	use({ "CRAG666/code_runner.nvim", requires = "nvim-lua/plenary.nvim" })
+	use({ "crag666/code_runner.nvim", requires = "nvim-lua/plenary.nvim" })
 
 	-- which key plugin
 	use({
@@ -41,7 +41,7 @@ packer.startup(function(use)
 
     -- harpoon
     use({
-        "ThePrimeagen/harpoon",
+        "theprimeagen/harpoon",
         config = function()
             require("harpoon").setup({
                 global_settings = {
@@ -49,7 +49,7 @@ packer.startup(function(use)
                     save_on_change = true,
                 },
                 projects = {
-                    ["/home/username/Projects"] = {
+                    ["/home/username/projects"] = {
                         term = {},
                         files = {},
                         git = {},
@@ -59,7 +59,7 @@ packer.startup(function(use)
         end,
     })
 
-	-- Color theme 1 (tokyonight-night)
+	-- color theme 1 (tokyonight-night)
 	-- use {
 	--     'folke/tokyonight.nvim',
 	--     requires = { 'tjdevries/colorbuddy.nvim' }
@@ -67,11 +67,11 @@ packer.startup(function(use)
 	--
 	-- vim.cmd [[colorscheme tokyonight-night]]
 
-	-- Color theme 2 (gruvbox)
+	-- color theme 2 (gruvbox)
 	-- use 'morhetz/gruvbox'
 	-- vim.cmd [[colorscheme gruvbox]]
 
-	-- -- Color theme 3 (everforest-hard)
+	-- -- color theme 3 (everforest-hard)
 	-- use 'sainnhe/everforest'
 	-- vim.g.everforest_background = 'soft'
 	-- vim.g.everforest_enable_italic = 1
@@ -79,7 +79,7 @@ packer.startup(function(use)
 	-- vim.g.everforest_better_performance = 1
 	-- vim.cmd [[colorscheme everforest]]
 
-	-- Color theme 4 (sonokai)
+	-- color theme 4 (sonokai)
 	-- use 'sainnhe/sonokai'
 	-- vim.g.sonokai_style = 'atlantis'
 	-- vim.g.sonokai_enable_italic = 1
@@ -87,19 +87,19 @@ packer.startup(function(use)
 	-- vim.g.sonokai_better_performance = 1
 	-- vim.cmd [[colorscheme sonokai]]
 
-	-- Color theme 5 (onedark)
+	-- color theme 5 (onedark)
 	-- use 'navarasu/onedark.nvim'
 	-- vim.cmd [[colorscheme onedark]]
 
-	-- Color theme 6 (dracula)
+	-- color theme 6 (dracula)
 	-- use 'dracula/vim'
 	-- vim.cmd [[colorscheme dracula]]
 
-	-- Color theme 7 (nightfox)
-	-- use 'EdenEast/nightfox.nvim'
+	-- color theme 7 (nightfox)
+	-- use 'edeneast/nightfox.nvim'
 	-- vim.cmd [[colorscheme nightfox]]
 
-	-- Color theme 8 {poimandres-olivercederborg}
+	-- color theme 8 {poimandres-olivercederborg}
 	-- use {
 	--     'olivercederborg/poimandres.nvim',
 	--     config = function()
@@ -114,18 +114,18 @@ packer.startup(function(use)
 	-- }
 	-- vim.cmd [[colorscheme poimandres]]
 
-	-- Color theme 9 (horizon)
-	use("Lunarvim/horizon.nvim")
+	-- color theme 9 (horizon)
+	use("lunarvim/horizon.nvim")
 	vim.cmd([[colorscheme horizon]])
 
-	use("lewis6991/gitsigns.nvim") -- Git signs
-	-- For commenting toggles
+	use("lewis6991/gitsigns.nvim") -- git signs
+	-- for commenting toggles
 	use("terrortylor/nvim-comment")
-	use("JoosepAlviste/nvim-ts-context-commentstring")
+	use("joosepalviste/nvim-ts-context-commentstring")
 
-	-- For auto saving
+	-- for auto saving
 	use({
-		"Pocco81/auto-save.nvim",
+		"pocco81/auto-save.nvim",
 		config = function()
 			require("auto-save").setup({
 				-- your config goes here
@@ -142,7 +142,7 @@ packer.startup(function(use)
 	--     tag = 'nightly' -- optional, updated every week. (see issue #1193)
 	-- }
 
-	-- Autoclose bracket pairs
+	-- autoclose bracket pairs
 	use({
 		"windwp/nvim-autopairs",
 		config = function()
@@ -150,10 +150,10 @@ packer.startup(function(use)
 		end,
 	})
 
-	-- Bufferline tabs
+	-- bufferline tabs
 	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "kyazdani42/nvim-web-devicons" })
 
-	-- Smooth scrolling
+	-- smooth scrolling
 	use("karb94/neoscroll.nvim")
 
 	-- use({
@@ -162,7 +162,7 @@ packer.startup(function(use)
 	-- 	requires = {
 	-- 		"nvim-lua/plenary.nvim",
 	-- 		"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-	-- 		"MunifTanjim/nui.nvim",
+	-- 		"muniftanjim/nui.nvim",
 	-- 		{
 	-- 			-- only needed if you want to use the commands with "_with_window_picker" suffix
 	-- 			"s1n7ax/nvim-window-picker",
@@ -187,19 +187,19 @@ packer.startup(function(use)
 	-- 		},
 	-- 	},
 	-- 	config = function()
-	-- 		-- Unless you are still migrating, remove the deprecated commands from v1.x
+	-- 		-- unless you are still migrating, remove the deprecated commands from v1.x
 	-- 		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 	--
-	-- 		-- If you want icons for diagnostic errors, you'll need to define them somewhere:
-	-- 		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-	-- 		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-	-- 		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-	-- 		vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
-	-- 		-- NOTE: this is changed from v1.x, which used the old style of highlight groups
-	-- 		-- in the form "LspDiagnosticsSignWarning"
+	-- 		-- if you want icons for diagnostic errors, you'll need to define them somewhere:
+	-- 		vim.fn.sign_define("diagnosticsignerror", { text = " ", texthl = "diagnosticsignerror" })
+	-- 		vim.fn.sign_define("diagnosticsignwarn", { text = " ", texthl = "diagnosticsignwarn" })
+	-- 		vim.fn.sign_define("diagnosticsigninfo", { text = " ", texthl = "diagnosticsigninfo" })
+	-- 		vim.fn.sign_define("diagnosticsignhint", { text = "", texthl = "diagnosticsignhint" })
+	-- 		-- note: this is changed from v1.x, which used the old style of highlight groups
+	-- 		-- in the form "lspdiagnosticssignwarning"
 	--
 	-- 		require("neo-tree").setup({
-	-- 			close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+	-- 			close_if_last_window = true, -- close neo-tree if it is the last window left in the tab
 	-- 			popup_border_style = "rounded",
 	-- 			enable_git_status = true,
 	-- 			enable_diagnostics = true,
@@ -223,39 +223,39 @@ packer.startup(function(use)
 	-- 					with_markers = true,
 	-- 					indent_marker = "│",
 	-- 					last_indent_marker = "└",
-	-- 					highlight = "NeoTreeIndentMarker",
+	-- 					highlight = "neotreeindentmarker",
 	-- 					-- expander config, needed for nesting files
 	-- 					with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
 	-- 					expander_collapsed = "",
 	-- 					expander_expanded = "",
-	-- 					expander_highlight = "NeoTreeExpander",
+	-- 					expander_highlight = "neotreeexpander",
 	-- 				},
 	-- 				icon = {
 	-- 					folder_closed = "",
 	-- 					folder_open = "",
 	-- 					folder_empty = "ﰊ",
-	-- 					-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+	-- 					-- the next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
 	-- 					-- then these will never be used.
 	-- 					default = "*",
-	-- 					highlight = "NeoTreeFileIcon",
+	-- 					highlight = "neotreefileicon",
 	-- 				},
 	-- 				modified = {
 	-- 					symbol = "[+]",
-	-- 					highlight = "NeoTreeModified",
+	-- 					highlight = "neotreemodified",
 	-- 				},
 	-- 				name = {
 	-- 					trailing_slash = false,
 	-- 					use_git_status_colors = true,
-	-- 					highlight = "NeoTreeFileName",
+	-- 					highlight = "neotreefilename",
 	-- 				},
 	-- 				git_status = {
 	-- 					symbols = {
-	-- 						-- Change type
+	-- 						-- change type
 	-- 						added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
 	-- 						modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
 	-- 						deleted = "✖", -- this can only be used in the git_status source
 	-- 						renamed = "", -- this can only be used in the git_status source
-	-- 						-- Status type
+	-- 						-- status type
 	-- 						untracked = "",
 	-- 						ignored = "",
 	-- 						unstaged = "",
@@ -276,22 +276,22 @@ packer.startup(function(use)
 	-- 						"toggle_node",
 	-- 						nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
 	-- 					},
-	-- 					["<2-LeftMouse>"] = "open",
+	-- 					["<2-leftmouse>"] = "open",
 	-- 					["<cr>"] = "open",
 	-- 					["<esc>"] = "revert_preview",
-	-- 					["P"] = { "toggle_preview", config = { use_float = true } },
-	-- 					["S"] = "open_split",
+	-- 					["p"] = { "toggle_preview", config = { use_float = true } },
+	-- 					["s"] = "open_split",
 	-- 					["s"] = "open_vsplit",
-	-- 					-- ["S"] = "split_with_window_picker",
+	-- 					-- ["s"] = "split_with_window_picker",
 	-- 					-- ["s"] = "vsplit_with_window_picker",
 	-- 					["t"] = "open_tabnew",
 	-- 					-- ["<cr>"] = "open_drop",
 	-- 					-- ["t"] = "open_tab_drop",
 	-- 					["w"] = "open_with_window_picker",
-	-- 					--["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
-	-- 					["C"] = "close_node",
+	-- 					--["p"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+	-- 					["c"] = "close_node",
 	-- 					["z"] = "close_all_nodes",
-	-- 					--["Z"] = "expand_all_nodes",
+	-- 					--["z"] = "expand_all_nodes",
 	-- 					["a"] = {
 	-- 						"add",
 	-- 						-- some commands may take optional config options, see `:h neo-tree-mappings` for details
@@ -299,7 +299,7 @@ packer.startup(function(use)
 	-- 							show_path = "none", -- "none", "relative", "absolute"
 	-- 						},
 	-- 					},
-	-- 					["A"] = "add_directory", -- also accepts the optional config.show_path option like "add".
+	-- 					["a"] = "add_directory", -- also accepts the optional config.show_path option like "add".
 	-- 					["d"] = "delete",
 	-- 					["r"] = "rename",
 	-- 					["y"] = "copy_to_clipboard",
@@ -314,7 +314,7 @@ packer.startup(function(use)
 	-- 					--}
 	-- 					["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
 	-- 					["q"] = "close_window",
-	-- 					["R"] = "refresh",
+	-- 					["r"] = "refresh",
 	-- 					["?"] = "show_help",
 	-- 					["<"] = "prev_source",
 	-- 					[">"] = "next_source",
@@ -326,7 +326,7 @@ packer.startup(function(use)
 	-- 					visible = true, -- when true, they will just be displayed differently than normal items
 	-- 					hide_dotfiles = false,
 	-- 					hide_gitignored = false,
-	-- 					hide_hidden = false, -- only works on Windows for hidden files/directories
+	-- 					hide_hidden = false, -- only works on windows for hidden files/directories
 	-- 					hide_by_name = {
 	-- 						--"node_modules"
 	-- 					},
@@ -338,14 +338,14 @@ packer.startup(function(use)
 	-- 						--".gitignored",
 	-- 					},
 	-- 					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-	-- 						--".DS_Store",
+	-- 						--".ds_store",
 	-- 						--"thumbs.db"
 	-- 					},
 	-- 					never_show_by_pattern = { -- uses glob style patterns
 	-- 						--".null-ls_*",
 	-- 					},
 	-- 				},
-	-- 				follow_current_file = false, -- This will find and focus the file in the active buffer every
+	-- 				follow_current_file = false, -- this will find and focus the file in the active buffer every
 	-- 				-- time the current file is changed while the tree is open.
 	-- 				group_empty_dirs = false, -- when true, empty folders will be grouped together
 	-- 				hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens neo-tree
@@ -353,15 +353,15 @@ packer.startup(function(use)
 	-- 				-- "open_current",  -- netrw disabled, opening a directory opens within the
 	-- 				-- window like netrw would, regardless of window.position
 	-- 				-- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-	-- 				use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+	-- 				use_libuv_file_watcher = false, -- this will use the os level file watchers to detect changes
 	-- 				-- instead of relying on nvim autocmd events.
 	-- 				window = {
 	-- 					mappings = {
 	-- 						["<bs>"] = "navigate_up",
 	-- 						["."] = "set_root",
-	-- 						["H"] = "toggle_hidden",
+	-- 						["h"] = "toggle_hidden",
 	-- 						["/"] = "fuzzy_finder",
-	-- 						["D"] = "fuzzy_finder_directory",
+	-- 						["d"] = "fuzzy_finder_directory",
 	-- 						["f"] = "filter_on_submit",
 	-- 						["<c-x>"] = "clear_filter",
 	-- 						["[g"] = "prev_git_modified",
@@ -370,7 +370,7 @@ packer.startup(function(use)
 	-- 				},
 	-- 			},
 	-- 			buffers = {
-	-- 				follow_current_file = true, -- This will find and focus the file in the active buffer every
+	-- 				follow_current_file = true, -- this will find and focus the file in the active buffer every
 	-- 				-- time the current file is changed while the tree is open.
 	-- 				group_empty_dirs = true, -- when true, empty folders will be grouped together
 	-- 				show_unloaded = true,
@@ -386,21 +386,22 @@ packer.startup(function(use)
 	-- 				window = {
 	-- 					position = "float",
 	-- 					mappings = {
-	-- 						["A"] = "git_add_all",
+	-- 						["a"] = "git_add_all",
 	-- 						["gu"] = "git_unstage_file",
 	-- 						["ga"] = "git_add_file",
 	-- 						["gr"] = "git_revert_file",
 	-- 						["gc"] = "git_commit",
 	-- 						["gp"] = "git_push",
 	-- 						["gg"] = "git_commit_and_push",
+	--                             ["<esc>"] = "close_window",
 	-- 					},
 	-- 				},
 	-- 			},
 	-- 			-- to close the tree when you open a file
-	-- 			vim.cmd("autocmd Filetype NvimTree setlocal nobuflisted"),
+	-- 			vim.cmd("autocmd filetype nvimtree setlocal nobuflisted"),
 	-- 		})
 	--
-	-- 		-- vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+	-- 		-- vim.cmd([[nnoremap \ :neotree reveal<cr>]])
 	-- 	end,
 	-- })
 end)
